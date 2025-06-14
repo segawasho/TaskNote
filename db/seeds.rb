f9e52f220afdd2999f0d4e3717f2bf4ebbf28abb
segawasho <segawasho0825pro@gmail.com>
2025-06-14 09:13:20 +0900

    デプロイ2seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,14 @@ industries = %w[
 ]
 
 ActiveRecord::Base.transaction do
+  # RoleCategory登録
   role_categories.each do |cat|
     rc = RoleCategory.find_or_initialize_by(name: cat[:name])
     rc.sort_order = cat[:sort_order]
     rc.save!
   end
 
+  # Role登録
   roles.each do |category_name, role_names|
     rc = RoleCategory.find_by!(name: category_name)
     role_names.each.with_index(1) do |role_name, i|
@@ -36,6 +38,7 @@ ActiveRecord::Base.transaction do
     end
   end
 
+  # Industry登録
   industries.each.with_index(1) do |name, i|
     ind = Industry.find_or_initialize_by(name: name)
     ind.sort_order = i
@@ -45,27 +48,20 @@ end
 
 puts "✅ 管理者ユーザーを作成します"
 
-[
-  {
-    name: '管理者A',
-    email: 'segawasho0825@gmail.com',
-    role_index: :first,
-    industry_index: :first
-  },
-  {
-    name: '管理者B',
-    email: 'segawasho0825pro@gmail.com',
-    role_index: :last,
-    industry_index: :last
-  }
-].each do |admin|
-  user = User.find_or_initialize_by(email: admin[:email])
-  user.name = admin[:name]
+# 管理者Aのみ登録（冪等性あり）
+admin_email = 'segawasho0825@gmail.com'
+admin_name = '管理者A'
+
+user = User.find_or_initialize_by(email: admin_email)
+if user.new_record?
+  user.name = admin_name
   user.password = 'password'
   user.password_confirmation = 'password'
   user.is_admin = true
-  user.role = Role.send(admin[:role_index])
-  user.industry = Industry.send(admin[:industry_index])
+  user.role = Role.first  # 会社員系の1番目が入る想定
+  user.industry = Industry.first
   user.save!
-  puts "✅ #{admin[:name]} 登録完了"
+  puts "✅ #{admin_name} 登録完了"
+else
+  puts "✅ #{admin_name} は既に存在します"
 end
